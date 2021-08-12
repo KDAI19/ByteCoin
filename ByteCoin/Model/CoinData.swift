e3cc09b5935a1f1740959314e81522815c5c60ee
Kaan DAI <kdai19@ku.edu.tr>
2021-08-12 05:23:03 -0700

    First Committ

---
+++ ByteCoin/Model/CoinData.swift
@@ -0,0 +1,14 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by Kaan Dai on 12.08.2021.
+//  Copyright © 2021 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData : Codable {
+    
+    let rate: Double
+}
